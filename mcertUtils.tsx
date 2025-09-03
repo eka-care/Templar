704e4bc4378c296b23eebb521a36a4bdab03f687
govind-eka <govind.ganeriwal@eka.care>
2025-09-03 17:35:56 +0530

    as discussed on call, made new file

---
+++ mcertUtils.tsx
@@ -0,0 +1,60 @@
+import { TemplateConfig } from './RenderPdfPrescription';
+import { getCustomFooterHtml, getFooterHtml, NO_FOOTER } from './templateUtils';
+import { DoctorProfile, LocalTemplateConfig } from './types';
+
+export const getBodyHtmlMCERT = (heading: string, description: string) => {
+    return (
+        <div>
+            <p
+                className="flex flex-col text-center text-28"
+                dangerouslySetInnerHTML={{
+                    __html: heading,
+                }}
+            ></p>
+
+            <p
+                className="mt-28"
+                dangerouslySetInnerHTML={{
+                    __html: description,
+                }}
+            ></p>
+        </div>
+    );
+};
+
+export const getFooterMcert = (
+    docProfile: DoctorProfile,
+    data: any,
+    renderPdfConfig?: TemplateConfig,
+    rxLocalConfig?: LocalTemplateConfig,
+    isHideFooterDetails?: boolean,
+): JSX.Element => {
+    if (renderPdfConfig?.footer_img) {
+        return getCustomFooterHtml(
+            docProfile,
+            data,
+            rxLocalConfig,
+            renderPdfConfig?.footer_top_margin,
+            renderPdfConfig?.footer_bottom_margin,
+            renderPdfConfig?.footer_left_margin,
+            renderPdfConfig?.footer_right_margin,
+            renderPdfConfig?.footer_img === NO_FOOTER ? undefined : renderPdfConfig?.footer_img,
+            renderPdfConfig?.show_signature,
+            renderPdfConfig?.show_name_in_signature,
+            renderPdfConfig?.show_signature_text,
+            renderPdfConfig?.show_page_number,
+            renderPdfConfig?.show_prescription_id,
+            renderPdfConfig.show_not_valid_for_medical_legal_purpose_message,
+            renderPdfConfig.show_eka_logo,
+            renderPdfConfig.attachment_image,
+            renderPdfConfig?.footer_doctor_name_color,
+            isHideFooterDetails,
+            undefined,
+            renderPdfConfig.show_approval_details,
+            renderPdfConfig.footer_height,
+            renderPdfConfig.floating_footer_details,
+        );
+    }
+
+    return getFooterHtml(docProfile, data, rxLocalConfig, renderPdfConfig);
+};
--- templateUtils.tsx
+++ templateUtils.tsx
@@ -33,7 +33,7 @@ import { padElements } from './padElementConfig';
 export const HEADER_CONTAINER = 'header_container';
 export const FOOTER_CONTAINER = 'footer_container';
 const NO_HEADER = 'no-header';
-const NO_FOOTER = 'no-footer';
+export const NO_FOOTER = 'no-footer';
 
 const fontSizeAccortingToType = {
     compact: '14px',
@@ -1200,26 +1200,6 @@ export const getSpacingBetweenSections = (
     }
 
     return spacingAccortingToType?.[spacing_between_sections];
-};
-
-export const getBodyHtmlMCERT = (heading: string, description: string) => {
-    return (
-        <div>
-            <p
-                className="flex flex-col text-center text-28"
-                dangerouslySetInnerHTML={{
-                    __html: heading,
-                }}
-            ></p>
-
-            <p
-                className="mt-28"
-                dangerouslySetInnerHTML={{
-                    __html: description,
-                }}
-            ></p>
-        </div>
-    );
 };
 
 export const getBodyHtml = (
@@ -10482,41 +10462,4 @@ export const getHeader = (
         data,
         rxConfig,
     );
-};
-
-export const getFooter = (
-    docProfile: DoctorProfile,
-    data: any,
-    renderPdfConfig?: TemplateConfig,
-    rxLocalConfig?: LocalTemplateConfig,
-    isHideFooterDetails?: boolean,
-): JSX.Element => {
-    if (renderPdfConfig?.footer_img) {
-        return getCustomFooterHtml(
-            docProfile,
-            data,
-            rxLocalConfig,
-            renderPdfConfig?.footer_top_margin,
-            renderPdfConfig?.footer_bottom_margin,
-            renderPdfConfig?.footer_left_margin,
-            renderPdfConfig?.footer_right_margin,
-            renderPdfConfig?.footer_img === NO_FOOTER ? undefined : renderPdfConfig?.footer_img,
-            renderPdfConfig?.show_signature,
-            renderPdfConfig?.show_name_in_signature,
-            renderPdfConfig?.show_signature_text,
-            renderPdfConfig?.show_page_number,
-            renderPdfConfig?.show_prescription_id,
-            renderPdfConfig.show_not_valid_for_medical_legal_purpose_message,
-            renderPdfConfig.show_eka_logo,
-            renderPdfConfig.attachment_image,
-            renderPdfConfig?.footer_doctor_name_color,
-            isHideFooterDetails,
-            undefined,
-            renderPdfConfig.show_approval_details,
-            renderPdfConfig.footer_height,
-            renderPdfConfig.floating_footer_details,
-        );
-    }
-
-    return getFooterHtml(docProfile, data, rxLocalConfig, renderPdfConfig);
 };
