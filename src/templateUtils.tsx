686aef758ef2471a604b57d4d00569aed1799f96
govind-eka <govind.ganeriwal@eka.care>
2025-10-15 22:56:59 +0530

    more changes

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -837,18 +837,32 @@ export const getCustomFooterHtml = (
                                     <div
                                         style={{
                                             width: '100%',
-                                            maxWidth: '5.75rem', // max-w-92
-                                            height: '3.75rem', // h-60
-                                            backgroundImage: `url(${docProfile?.profile?.professional?.signature})`,
-                                            backgroundSize: 'contain',
-                                            backgroundPosition: 'center',
-                                            backgroundRepeat: 'no-repeat',
+                                            maxWidth: '5.75rem', // same as max-w-92
+                                            height: '3.8rem', // same as h-60
+                                            position: 'relative',
+                                            overflow: 'hidden',
+                                            paddingBottom: '2px',
+                                            boxSizing: 'border-box',
                                         }}
-                                    />
-                                    // <img
-                                    //     src={docProfile?.profile?.professional?.signature}
-                                    //     className="max-w-92 h-60 object-fit-contain"
-                                    // />
+                                    >
+                                        <img
+                                            src={docProfile?.profile?.professional?.signature}
+                                            alt="Doctor Signature"
+                                            style={{
+                                                position: 'absolute',
+                                                top: 0,
+                                                left: 0,
+                                                right: 0,
+                                                bottom: 0,
+                                                margin: 'auto',
+                                                maxWidth: '100%',
+                                                maxHeight: '100%',
+                                                width: 'auto',
+                                                height: 'auto',
+                                                display: 'block',
+                                            }}
+                                        />
+                                    </div>
                                 )}
                                 {show_name_in_signature && (
                                     <p
