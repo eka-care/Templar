e72702d736f8b074523eea60198e02ad682bc21c
Vishesh Sharma <vishesh.sharma@eka.care>
2025-11-05 15:50:04 +0530

    test comment

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -1852,7 +1852,7 @@ export const getFooterHtml = (
         </>
     );
 };
-
+//test
 export const getDoubleColumnMedications = (
     d: RenderPdfPrescription,
     render_pdf_config?: TemplateConfig,
