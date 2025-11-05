fddaa607403396c7c94e108d7e9afbae3d99bf9f
vishesh-eka <vishesh.sharma@eka.care>
2025-11-05 14:06:33 +0530

    Add logs (#44)
    * adding comment for testing
    
    * remove comment
    
    * remove comment
    
    * removing logs
    
    * removing logs

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -1852,7 +1852,7 @@ export const getFooterHtml = (
         </>
     );
 };
-//random test comment
+//here we go
 export const getDoubleColumnMedications = (
     d: RenderPdfPrescription,
     render_pdf_config?: TemplateConfig,
