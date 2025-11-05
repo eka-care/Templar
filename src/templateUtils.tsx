7f492c22b2a3ae005b58ab84a9605e312220202e
vishesh-eka <vishesh.sharma@eka.care>
2025-11-05 13:58:25 +0530

    Add GitHub Actions workflow for build and submodule bump (#41)
    * Add GitHub Actions workflow for build and submodule bump
    
    This workflow automates the build, test, and submodule bumping process for the Templar project. It includes steps to check out code, set up Node.js, build the package, and create pull requests for submodule updates.
    
    * testing with branch first
    
    * adding comment for testing (#42)
    
    * Modify bump workflow condition for branch triggers
    
    Updated bump workflow to allow triggering from multiple branches.
    
    * Add logs (#43)
    
    * adding comment for testing
    
    * remove comment
    
    * remove comment
    
    * Change npm command from ci to install
    
    ---------
    
    Co-authored-by: Vishwesh Jainkuniya <vishwesh.jainkuniya@eka.care>

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -1852,7 +1852,7 @@ export const getFooterHtml = (
         </>
     );
 };
-
+//random test comment
 export const getDoubleColumnMedications = (
     d: RenderPdfPrescription,
     render_pdf_config?: TemplateConfig,
