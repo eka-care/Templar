4629a74cbce5b22cf4265e0230b6edbd2df1c777
Vishesh Sharma <vishesh.sharma@eka.care>
2025-11-05 15:20:11 +0530

    remove logs

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -1852,7 +1852,7 @@ export const getFooterHtml = (
         </>
     );
 };
-//k
+
 export const getDoubleColumnMedications = (
     d: RenderPdfPrescription,
     render_pdf_config?: TemplateConfig,
