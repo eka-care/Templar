4d3454616e59bb81a49ced63ee88b64c96d35246
Supratim Malakar <supratim.malakar@eka.care>
2026-04-09 15:32:56 +0530

    fix signature show flag

--- src/templateUtils.tsx
+++ src/templateUtils.tsx
@@ -1908,7 +1908,7 @@ export const getFooterHtml = (
                                     boxSizing: 'border-box',
                                 }}
                             >
-                                {docProfile?.profile?.professional?.signature && (
+                                {renderPdfConfig?.show_signature && docProfile?.profile?.professional?.signature && (
                                     <img
                                         src={docProfile?.profile?.professional?.signature + `?t=${new Date().getTime()}`}
                                         alt="Doctor Signature"
@@ -1938,9 +1938,11 @@ export const getFooterHtml = (
                                 {docProfile?.profile?.personal?.name?.f || ''}{' '}
                                 {docProfile?.profile?.personal?.name?.l || ''}
                             </p>
-                            <span className="whitespace-preline text-right">
-                                {docProfile?.profile?.professional?.signature_text || ''}
-                            </span>
+                            {renderPdfConfig?.show_signature_text && (
+                                <span className="whitespace-preline text-right">
+                                    {docProfile?.profile?.professional?.signature_text || ''}
+                                </span>
+                            )}
                         </div>
                     </div>
 
